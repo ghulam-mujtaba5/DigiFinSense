9ade08c64aab87113ed95dc3c7029851e94d0706
Ghulam Mujtaba <ghulammujtaba1005@gmail.com>
2025-01-05 16:31:09 +0500

    Remove commented-out code and add react-native-url-polyfill dependency

--- App.tsx
+++ App.tsx
@@ -1,39 +1,5 @@
 
-
-// import React from 'react';
-// import {SafeAreaView, StatusBar, StyleSheet, useColorScheme} from 'react-native';
-// import SignupScreen from './src/screens/SignupScreen'; // Import SignupScreen
-
-// function App(): React.JSX.Element {
-//   const isDarkMode = useColorScheme() === 'dark';
-
-//   const backgroundStyle = {
-//     backgroundColor: isDarkMode ? '#333333' : '#F5F5F5',
-//     flex: 1,
-//   };
-
-//   return (
-//     <SafeAreaView style={backgroundStyle}>
-//       <StatusBar
-//         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-//         backgroundColor={backgroundStyle.backgroundColor}
-//       />
-//       {/* Render SignupScreen */}
-//       <SignupScreen />
-//     </SafeAreaView>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
-
-// export default App;
-
+//App.tsx
 
 import React from 'react';
 import { SafeAreaView, StatusBar, StyleSheet } from 'react-native';
@@ -55,3 +21,6 @@ const styles = StyleSheet.create({
 });
 
 export default App;
+
+
+
--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+// /**
+//  * @format
+//  */
+// //index.js
+// import {AppRegistry} from 'react-native';
+// import App from './App';
+// import {name as appName} from './app.json';
+
+// AppRegistry.registerComponent(appName, () => App);
+
+// Import the polyfill at the top of index.js
+import 'react-native-url-polyfill/auto';
+
 /**
  * @format
  */
